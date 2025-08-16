113989c86db08ae4c7678f5f76b465efa0a94451
vihooo <vihooo520@163.com>
2025-08-16 17:08:18 +0800

    logo欢迎页、关于我的页

---
+++ sxunt_photography/pages/center/center.js
@@ -0,0 +1,82 @@
+//const WXAPI = require('apifm-wxapi')
+Page({
+
+  
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  async onShow () {
+    let fileNumber = 10;
+    let newsNumber = 20;
+    let views = 30;
+    this.setData({
+      mylogo: '../../assets/images/logoxx.jpg',
+      myname: 'vihooo',
+      fileNumber,
+      newsNumber,
+      views,
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+  calling(){
+    wx.makePhoneCall({
+      phoneNumber: wx.getStorageSync('tel')
+    })
+  }
+})
---
+++ sxunt_photography/pages/logo/logo.js
@@ -0,0 +1,86 @@
+// pages/logo/logo.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+ data:{
+    time: 1
+  },
+
+  bindLogoTap: function() {
+    wx.reLaunch({
+      url: '../index/index'
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 设置初始计时秒数
+    let time = 1;
+    // 开始定时器
+    this.timer = setInterval(() => {
+      this.setData({
+        time: -- time
+      });
+      // 读完秒后携带洗衣机编号跳转到计费页
+      if(time < 0){
+        clearInterval(this.timer)
+        wx.reLaunch({
+          url: '../index/index'
+        })
+      }
+    },1000)
+   
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
